de11b55670529b9c917722a3b14cbde11f2b148d
Ropung <563710@naver.com>
2023-06-05 02:15:13 +0900

    initial commit

---
+++ springboot-root/src/main/java/com/joara/root/JoaraSpringbootRootApplication.java
@@ -0,0 +1,13 @@
+package com.joara.root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaraSpringbootRootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaraSpringbootRootApplication.class, args);
+	}
+
+}
