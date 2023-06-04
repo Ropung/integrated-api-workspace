9ffa4220d17b9ad7a0e976a143357a3d94f07b31
Ropung <563710@naver.com>
2023-06-05 06:46:37 +0900

    feat(flyway): 상수관리

---
+++ springboot-root/base-modules/joara-integrated-flyway/src/main/java/com/joara/rdb/JoaraPostgresSchemaConstants.java
@@ -0,0 +1,19 @@
+package com.joara.rdb;
+
+public final class JoaraPostgresSchemaConstants {
+	public static final String SCHEMA = "joara_basic";
+	public static final String TB_Refresh = "refresh_token";
+	public static final String TB_MEMBER = "member";
+	public static final String TB_FAVOR_GENRE = "member_favor_genre";
+	public static final String TB_FAVOR_BOOK = "member_favor_book";
+	public static final String TB_BOOK_GENRE = "book_genre";
+	public static final String TB_BOOK = "book";
+	public static final String TB_BOOK_TAG = "book_tag";
+	public static final String TB_EPI_LOVE = "epi_love";
+	public static final String TB_EPI = "epi";
+	public static final String TB_EPI_REPLY = "epi_reply";
+	public static final String TB_EPI_SUB_REPLY = "epi_sub_reply";
+	private JoaraPostgresSchemaConstants() {
+	
+	}
+}
