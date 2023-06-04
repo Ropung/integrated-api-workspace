e24f42e517eb3132d0d2fbfa938a069ca3c709ef
Ropung <563710@naver.com>
2023-06-05 06:49:45 +0900

    feat(service-domain): Vo,jpa 추가

---
+++ springboot-root/authentication-service/domain/src/main/java/com/joara/domain/model/Member.java
@@ -0,0 +1,32 @@
+package com.joara.domain.model;
+
+import com.joara.domain.model.type.AccountStatus;
+import com.joara.domain.model.type.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Builder
+public class Member {
+    public UUID id;
+    public String email;
+    public String password;
+    public String name;
+    public String nickname;
+    public String phone;
+    public Gender gender;
+    public OffsetDateTime birth;
+    public OffsetDateTime createdAt;
+    public OffsetDateTime updatedAt;
+    public OffsetDateTime deletedAt;
+    public AccountStatus status;
+}
---
+++ springboot-root/authentication-service/domain/src/main/java/com/joara/domain/repository/Members.java
@@ -0,0 +1,9 @@
+package com.joara.domain.repository;
+
+import com.joara.domain.model.Member;
+import com.joara.support.repository.BaseRepository;
+
+import java.util.UUID;
+
+public interface Members extends BaseRepository<Member, UUID> {
+}
