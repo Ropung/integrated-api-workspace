6035ccb4c5ff895c23de2738928a06e9eb235c0c
Gihun <563710@naver.com>
2023-06-12 15:08:25 +0900

    feat(properties): Add jwt, cors, upload

--- springboot-root/authentication-service/application/src/main/java/com/joara/auth/properties/jwt/JwtProperties.java
+++ springboot-root/authentication-service/application/src/main/java/com/joara/auth/properties/jwt/JwtProperties.java
@@ -7,9 +7,10 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesBindin
 @ConfigurationPropertiesBinding
 public record JwtProperties(
 		//1. 토큰 Properties에서 jwt 비밀번호를 가져온다.
-		String password
+		String secret,
+		Long expiredIn
 ) {
 	public JwtProperties {
-		if (password == null) password = "77d1b474a0920ab13e44a05170117cf0e809bad5c554d19020a95b45e9e2fb95893b8b149382e294d78fdb8e5aa2ae266b5797d985f5dc127366d2a50ec3938e";
+		if (expiredIn == null) expiredIn = 1_800_000L;
 	}
 }
