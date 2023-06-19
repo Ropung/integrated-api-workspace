92712694265356f271ff354ec757d2e83facd3e2
dh2wo <osj4927@gmail.com>
2023-06-19 21:39:23 +0900

    chore(book): QueryRepository 상속받았으니 중복매서드 존재하는거 제거

--- springboot-root/book-service/application/src/main/java/com/joara/book/repository/BookQueryRepository.java
+++ springboot-root/book-service/application/src/main/java/com/joara/book/repository/BookQueryRepository.java
@@ -10,5 +10,8 @@ import java.util.Optional;
 public interface BookQueryRepository extends BaseCommandRepository<Book, Long> {
     // List<Member> findAll(Pageable pageable);  // Select 1번 = 데이터(.044))
     Page<Book> findAll(Pageable pageable);  // Select 2번 = 데이터(.044) + 카운트를 매번 함(.071) -> (.12)
-    Optional<Book> findById(Long id);
+
+    // findById 매서드의 경우 상속받았기 떄문에 쓸수있다.
+
+
 }
