d318cfd3db36edb52a1c171655f1589ed113bd91
dh2wo <osj4927@gmail.com>
2023-06-26 10:23:36 +0900

    fix(Episode): Add updatedAt, deletedAt(ReadModel, Projection), create (viewCount, heartCount) default 0L

--- springboot-root/book-service/application/read-model/src/main/java/com/joara/episode/domain/model/EpisodeReadModel.java
+++ springboot-root/book-service/application/read-model/src/main/java/com/joara/episode/domain/model/EpisodeReadModel.java
@@ -3,16 +3,21 @@ package com.joara.episode.domain.model;
 import lombok.Builder;
 
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 public record EpisodeReadModel() {
     @Builder
     public record EpisodeListViewReadModel(
+            UUID id,
             Long bookId,
             Long epiNum,
+            String nickname,
             String epiTitle,
             Long viewCount,
             Long heartCount,
-            OffsetDateTime createdAt
+            OffsetDateTime createdAt,
+            OffsetDateTime updatedAt,
+            OffsetDateTime deletedAt
 
     ){}
 }
