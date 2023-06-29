75cc9ccef73ad13c97c093c6fc692063fd4c034b
qhtneo <hj9671@gmail.com>
2023-06-29 23:54:51 +0900

    feat(episode): query when episode status is active

--- springboot-root/book-service/application/domain/src/main/java/com/joara/book/domain/model/episode/type/EpisodeStatus.java
+++ springboot-root/book-service/application/domain/src/main/java/com/joara/book/domain/model/episode/type/EpisodeStatus.java
@@ -1,14 +1,34 @@
 package com.joara.book.domain.model.episode.type;
 
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 public enum EpisodeStatus {
 	/** 임시 저장 등 */
-	PENDING,
+	PENDING(false),
 	/** 활성화(Normal) */
-	ACTIVE,
+	ACTIVE(true),
 	/** Episode가 실제로 삭제 됐을 때 */
-	REMOVED,
+	REMOVED(false),
 	/** 상위 Book이 삭제 됐을 때 등 */
-	DISABLED,
+	DISABLED(false),
 	/** 블락 */
-	BLOCK
+	BLOCK(false);
+
+	public final boolean readable;
+
+	public static List<EpisodeStatus> readableItems() {
+		List<EpisodeStatus> readableItems = new ArrayList<>();
+
+		for (var status: values()) {
+			if (status.readable) {
+				readableItems.add(status);
+			}
+		}
+
+		return readableItems;
+	}
 }
